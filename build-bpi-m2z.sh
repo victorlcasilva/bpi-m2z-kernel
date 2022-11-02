5aed9ddff3ad8754c62f4f1d51fe9dba1788915d
Victor LCA Silva <victor.the008@gmail.com>
2022-11-02 17:29:16 -0300

    Update build script to include config + build time

--- build-bpi-m2z.sh
+++ build-bpi-m2z.sh
@@ -6,10 +6,14 @@ if [ "$1" = "docker" ]; then
     CONFIG=m2z_docker_defconfig
 fi
 
+echo ""
+echo "  BUILD CONFIG: $CONFIG"
+echo ""
+
 BUILDTIMESTAMP=$(date +%Y%m%d)
 
 rm -rf output
 make ARCH=arm clean
-make ARCH=arm LOCALVERSION=-$BUILDTIMESTAMP CROSS_COMPILE=arm-none-eabi- INSTALL_MOD_PATH=output -j8 $CONFIG zImage modules modules_install dtbs
+time make ARCH=arm LOCALVERSION=-$BUILDTIMESTAMP CROSS_COMPILE=arm-none-eabi- INSTALL_MOD_PATH=output -j8 $CONFIG zImage modules modules_install dtbs
 
 sh dist-bpi-m2z.sh
