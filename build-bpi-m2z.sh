670cfd76581286c2b36be1cc794791708c1f1a38
Victor LCA Silva <victor.the008@gmail.com>
2022-11-02 16:33:26 -0300

    Update build script to include docker config + append timestamp to kernel release string

--- build-bpi-m2z.sh
+++ build-bpi-m2z.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
+CONFIG=m2z_lima_defconfig
+
+if [ "$1" = "docker" ]; then
+    CONFIG=m2z_docker_defconfig
+fi
+
+BUILDTIMESTAMP=$(date +%Y%m%d)
+
+rm -rf output
 make ARCH=arm clean
-make ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabi- INSTALL_MOD_PATH=output -j8 m2z_lima_defconfig zImage modules modules_install dtbs
+make ARCH=arm LOCALVERSION=-$BUILDTIMESTAMP CROSS_COMPILE=arm-none-eabi- INSTALL_MOD_PATH=output -j8 $CONFIG zImage modules modules_install dtbs
