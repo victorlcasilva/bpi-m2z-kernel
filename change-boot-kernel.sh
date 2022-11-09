f757ca05a1c13f08ec17dd4afbc68e17b4e73c19
Victor LCA Silva <victor.the008@gmail.com>
2022-11-09 10:56:46 -0300

    Refactor script to change boot symlinks

--- change-boot-kernel.sh
+++ change-boot-kernel.sh
@@ -1,22 +1,49 @@
 #!/bin/sh
 
 usage() {
-    echo "\n  Usage: $0 version [-check]\n"
+    echo "\n  Usage: $0 kernelVersion [-check]\n"
 }
 
 checkFileExists() {
     CHECKFILE=$1
+    echo "  - Checking file $CHECKFILE"
     if [ ! -f $CHECKFILE ]; then
-        echo "\n  ERROR: Couldn't find file $CHECKFILE"
+        echo "\n  ERROR: Couldn't find file $CHECKFILE in $CURRENT_DIR"
         usage
         exit 1
     fi
 }
 
+removeExistingSymLinks() {
+    SYMLINK=$1
+    if [ -f $SYMLINK ]; then
+        if [ -z $READONLY ]; then
+            echo "  - Remove existing symlink $SYMLINK"
+            rm -f $SYMLINK
+        else
+            echo "  - Should remove existing symlink $SYMLINK"
+        fi
+    fi
+}
+
+createSymLink() {
+    SYMLINK=$1
+    SYMTARGET=$2
+    if [ -z $READONLY ]; then
+        echo "  - Creating symlink $SYMLINK -> $SYMTARGET"
+        ln -s $SYMTARGET $SYMLINK
+    else
+        echo "  - Should create symlink $SYMLINK -> $SYMTARGET"
+    fi
+}
+
 if [ -z $1 ]; then
-    echo "\n  ERROR: No version specified"
     usage
     exit 1
+fi
+
+if [ "$2" = "-check" ]; then
+    READONLY=1
 fi
 
 VERSION=$1
@@ -27,21 +54,14 @@ LINK_IMG=zImage
 DTB=bpi-m2-zero-v4.dtb_${VERSION}
 LINK_DTB=bpi-m2-zero.dtb
 
+CURRENT_DIR=$(pwd)
+
+echo "check:"
 checkFileExists $IMG
 checkFileExists $DTB
 
-if [ "$2" = "-check" ]; then
-    echo "> Remove $LINK_IMG"
-    echo "> Remove $LINK_DTB"
-    echo "> Create symlink $LINK_IMG -> $IMG"
-    echo "> Create symlink $LINK_DTB -> $DTB"
-else
-    echo "Updating boot kernel to version $VERSION"
-    echo "Removing existing links"
-    rm -f $LINK_IMG
-    rm -f $LINK_DTB
-    echo "Creating new links"
-    ln -s $IMG $LINK_IMG
-    ln -s $DTB $LINK_DTB
-    echo "Done!"
-fi
+echo "\nupdate:"
+removeExistingSymLinks $LINK_IMG
+removeExistingSymLinks $LINK_DTB
+createSymLink $LINK_IMG $IMG
+createSymLink $LINK_DTB $DTB
